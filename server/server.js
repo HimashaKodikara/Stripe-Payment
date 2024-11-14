ae344e0d40f058375fe30e89b5f427a12f7617de
19840769 <kodikarah.21@uom.lk>
2024-11-14 23:46:01 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,44 +1,39 @@
 require('dotenv').config();
 
+const express = require("express");
+const app = express();
 
-
-const express =require("express")
-const app= express()
-
-app.use(express.json())
-app.use(express.static("client"))
+app.use(express.json());
+app.use(express.static("client"));
 
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
-const storeItems = new Map([
-    [1,{priceInCents:10000,name:'Learn React Today'}],
-    [2,{priceInCents:20000,name:'Learn CSS Today'}]
-])
 
 app.post("/create-checkout-session", async (req, res) => {
     try {
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
+        payment_method_types: ["card"], // Card payment only
         mode: "payment",
-        line_items: req.body.items.map(item => {
-          const storeItem = storeItems.get(item.id)
-          return {
+        line_items: [
+          {
             price_data: {
               currency: "usd",
               product_data: {
-                name: storeItem.name,
+                name: 'Payment', // Generic name
               },
-              unit_amount: storeItem.priceInCents,
+              unit_amount: 15000, // Set amount
             },
-            quantity: item.quantity,
-          }
-        }),
+            quantity: 1,
+          },
+        ],
         success_url: `${process.env.SERVER_URL}/success.html`,
         cancel_url: `${process.env.SERVER_URL}/cancel.html`,
-      })
-      res.json({ url: session.url })
+      });
+      res.json({ url: session.url });
     } catch (e) {
-      res.status(500).json({ error: e.message })
+      res.status(500).json({ error: e.message });
     }
-  })
-app.listen(3000)
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
