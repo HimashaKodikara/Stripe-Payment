f058b95d798769ca9469ac8ec845638fa253ee08
19840769 <kodikarah.21@uom.lk>
2024-11-14 23:16:48 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -8,15 +8,37 @@ const app= express()
 app.use(express.json())
 app.use(express.static("client"))
 
-const stripe = require('stripe')("sk_test_51QJef7RwP0CS6vlp1xPmO3Nre0XQtp4VMvgKfYvMWeipbCfvV38ECuNVUOTiOHPikUYDhFSWh1gkhhlYE8LfLUEN00x5umMnMs"
-);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const storeItems = new Map([
     [1,{priceInCents:10000,name:'Learn React Today'}],
     [2,{priceInCents:20000,name:'Learn CSS Today'}]
 ])
 
-app.post('/create-checkout-session',(req,res)=>{
-    res.json({url:'Hi'})
-})
+app.post("/create-checkout-session", async (req, res) => {
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        mode: "payment",
+        line_items: req.body.items.map(item => {
+          const storeItem = storeItems.get(item.id)
+          return {
+            price_data: {
+              currency: "usd",
+              product_data: {
+                name: storeItem.name,
+              },
+              unit_amount: storeItem.priceInCents,
+            },
+            quantity: item.quantity,
+          }
+        }),
+        success_url: `${process.env.SERVER_URL}/success.html`,
+        cancel_url: `${process.env.SERVER_URL}/cancel.html`,
+      })
+      res.json({ url: session.url })
+    } catch (e) {
+      res.status(500).json({ error: e.message })
+    }
+  })
 app.listen(3000)
